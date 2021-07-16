606cc0bd99c03cedda62426c2f26ff278e89d5d8
Steven Webster <30727673+webslinger74@users.noreply.github.com>
2021-07-16 14:05:43 +0100

    Create script.sh

---
+++ scripts/terraform/script.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+
+echo "apt install docker starting"
+# apt install -y docker
+# echo "docker installed"
+# service docker start
+# echo "docker daemon started"
+# echo "starting building image"
+# docker build .
+# echo "ending building image"
