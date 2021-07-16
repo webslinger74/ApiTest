7500a4ed0806dd282eb99de6c0b96122ef75926c
Steven Webster <30727673+webslinger74@users.noreply.github.com>
2021-07-16 23:16:13 +0100

    Update script.sh

--- scripts/terraform/script.sh
+++ scripts/terraform/script.sh
@@ -1,10 +1,11 @@
 !#/bin/bash
 
 echo "apt install docker starting"
-# apt install -y docker
-# echo "docker installed"
-# service docker start
-# echo "docker daemon started"
+sudo yum update
+sudo yum install -y docker
+echo "docker installed"
+service docker start
+echo "docker daemon started"
 # echo "starting building image"
 # docker build .
 # echo "ending building image"
