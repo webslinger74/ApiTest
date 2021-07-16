176dd078e0d46c36164f8f771e169f001f017a3c
Steven Webster <30727673+webslinger74@users.noreply.github.com>
2021-07-16 13:36:01 +0100

    Update variables.tf

--- scripts/terraform/variables.tf
+++ scripts/terraform/variables.tf
@@ -1,9 +1,9 @@
 variable "PATH_TO_PUBLIC_KEY" {
-    default = "stekeyterra.pub" 
+    default = "/var/jenkins_home/.ssh/my_aws.pub" 
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
-    default = "stekeyterra"
+    default = "/var/jenkins_home/.ssh/my_aws"
 }
 
 variable "INSTANCE_USERNAME" {
