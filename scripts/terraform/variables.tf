3be41a04363b477be6efa554ded2f1cee9548799
Steven Webster <30727673+webslinger74@users.noreply.github.com>
2021-07-16 13:22:37 +0100

    Create variables.tf

---
+++ scripts/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "PATH_TO_PUBLIC_KEY" {
+    default = "stekeyterra.pub" 
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+    default = "stekeyterra"
+}
+
+variable "INSTANCE_USERNAME" {
+    default = "ec2-user"
+}
