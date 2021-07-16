3f57e992245fa9f72780ff254311ddb5ba14544d
Steven Webster <30727673+webslinger74@users.noreply.github.com>
2021-07-16 13:21:22 +0100

    Update main.tf

--- scripts/terraform/main.tf
+++ scripts/terraform/main.tf
@@ -1,1 +1,23 @@
+provider "aws" {
+  profile = "default
+  region = "us-east-2"
+}
 
+resource "aws_instance" "server1" {
+  ami = "ami-0233c2d874b811deb"
+  instance_type= "t2.micro"
+  tags = {
+    Name = "terraform-example"
+}
+  key_name = aws_key_pair.stekeyterra.key_name
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+}
+
+resource "aws_key_pair "skekeyterra" {
+  key_name = "steekeyterra"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+   
