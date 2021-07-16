af8bb4928d9c0cf0e43996496f707c4968b5b664
Steven Webster <30727673+webslinger74@users.noreply.github.com>
2021-07-16 13:21:46 +0100

    Update main.tf

--- scripts/terraform/main.tf
+++ scripts/terraform/main.tf
@@ -17,7 +17,7 @@ resource "aws_security_group" "instance" {
 }
 
 resource "aws_key_pair "skekeyterra" {
-  key_name = "steekeyterra"
+  key_name = "stekeyterra"
   public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
    
