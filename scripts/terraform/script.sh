ea0a3460e237d7d5201c707bb835f9ce598590f8
Steven Webster <30727673+webslinger74@users.noreply.github.com>
2021-07-16 23:45:04 +0100

    Update script.sh

--- scripts/terraform/script.sh
+++ scripts/terraform/script.sh
@@ -4,7 +4,7 @@ echo "apt install docker starting"
 sudo yum update
 sudo yum install -y docker
 echo "docker installed"
-service docker start
+sudo service docker start
 echo "docker daemon started"
 sudo docker build -t some-content-nginx ../.
-docker run --name some-nginx -d -p 8080:80 some-content-nginx
+sudo docker run --name some-nginx -d -p 8080:80 some-content-nginx
