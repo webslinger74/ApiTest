4265074d8489d5ce80ef594063af235da6b177de
Steven Webster <30727673+webslinger74@users.noreply.github.com>
2021-07-17 13:26:47 +0100

    Update outputs.tf

--- scripts/terraform/outputs.tf
+++ scripts/terraform/outputs.tf
@@ -1,3 +1,14 @@
 output "public_ip_address" {
     value = aws_instance.server1.public_ip
 }
+
+
+output "needed_for_ansible" {
+    value = locals.someVariable
+}
+
+locals {
+    someVariable = templatefile("${./template.tpl", {
+        ec2_ip = aws_instance.server1.public_ip
+    })
+}
